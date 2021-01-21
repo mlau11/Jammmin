47fb89cfa6c3c58e3991639caaccf512a45479fc
Michael Lau <michael@PassionGrowers.com>
2021-01-20 21:09:51 -0500

    animated placeholder text for search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -21,16 +21,39 @@ class SearchBar extends React.Component {
     }
 
     handleEnter(e) {
-        console.log('entered');
         if(e.key === 'Enter'){
             this.search();
         }
     }
 
+    componentDidMount() {
+        let searchBar = document.getElementById("searchBar");
+        var txt = "Enter A Song, Album, or Artist";
+        var timeOut;
+        var txtLen = txt.length;
+        var char = 0;
+        searchBar.setAttribute('placeholder', '|');
+        (function typeIt() {
+            var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+            timeOut = setTimeout(function () {
+                char++;
+                var type = txt.substring(0, char);
+                searchBar.setAttribute('placeholder', type + '|');
+                typeIt();
+
+                if (char == txtLen) {
+                    searchBar.setAttribute('placeholder', type) // remove the '|'
+                    clearTimeout(timeOut);
+                }
+
+            }, humanize);
+        }());
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input id='searchBar' type='text' placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleEnter}/>
+                <input id='searchBar' type='text' placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleEnter} />
                 <button className="SearchButton" onClick={this.search}>SEARCH</button> 
             </div>
         );
